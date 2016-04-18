5a3902eeef0201e6df046fddd94405e96ba88c44
Colin Hoglund <colinhoglund@gmail.com>
2016-04-18 10:46:51 -0400

    use consistent naming in defaults

--- yaml-vagrant/settings.rb
+++ yaml-vagrant/settings.rb
@@ -1,8 +1,12 @@
 require 'yaml'
 
 class Settings
+  attr_reader :settings
+
   def initialize(settings)
+    # load settings hash
     @settings = settings
+    # set undefined defaults
     set_defaults(YAML.load_file('yaml-vagrant/defaults.yml'))
   end
 
@@ -10,18 +14,17 @@ class Settings
     return new(settings).settings
   end
 
-  attr_reader :settings
-
   private
   def set_defaults(defaults)
+    # set base defaults where not defined
     defaults.each do |k,v|
       @settings[k] ||= v
     end
+    # set vm defaults where not defined
     @settings['vms'].each do |vm|
-      defaults['vm'].each do |k,v|
+      defaults['vms'].each do |k,v|
         vm[k] ||= v
       end
     end
-    @settings.delete('vm')
   end
 end
